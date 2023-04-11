245d092dc5d7d4239bf2c28054cc8e905bddc4d5
Loïc PALAYER <84199585+loicpalayer@users.noreply.github.com>
2023-04-11 16:24:17 +0200

    refactor: remove useless if statement

--- app/src/main/java/com/kfc/restorater/RestoApplication.java
+++ app/src/main/java/com/kfc/restorater/RestoApplication.java
@@ -3,7 +3,6 @@ package com.kfc.restorater;
 import android.app.Application;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.os.Build;
 
 public class RestoApplication extends Application {
     private static final String CHANNEL_ID = "login_notification_channel";
@@ -12,11 +11,9 @@ public class RestoApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Login Notification", NotificationManager.IMPORTANCE_HIGH);
-            notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Login Notification", NotificationManager.IMPORTANCE_HIGH);
+        notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
     }
 
 }
