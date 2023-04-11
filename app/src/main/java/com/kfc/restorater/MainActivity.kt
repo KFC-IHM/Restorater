51b07c67212751bbd99d80ce3b56024a65ad6569
Ozeliurs <ozeliurs@gmail.com>
2023-04-11 17:07:22 +0200

    feat: add listeners
    Ref: #2

--- app/src/main/java/com/kfc/restorater/MainActivity.kt
+++ app/src/main/java/com/kfc/restorater/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kfc.restorater
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +9,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listMenu = findViewById<ImageButton>(R.id.list_menu)
+        listMenu.setOnClickListener {
+            setContentView(R.layout.activity_main)
+        }
+
+        val mapMenu = findViewById<ImageButton>(R.id.map_menu)
+        mapMenu.setOnClickListener {
+            setContentView(R.layout.activity_map)
+        }
+
+        val userMenu = findViewById<ImageButton>(R.id.user_menu)
+        userMenu.setOnClickListener {
+            setContentView(R.layout.activity_user)
+        }
     }
 }
