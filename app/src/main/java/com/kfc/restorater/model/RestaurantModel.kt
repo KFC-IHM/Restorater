e65e29363e1677ce1e9b2c64dfc4c45fde51b450
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-05-09 15:48:42 +0200

    feat: restaurant model parcelable

--- app/src/main/java/com/kfc/restorater/model/RestaurantModel.kt
+++ app/src/main/java/com/kfc/restorater/model/RestaurantModel.kt
@@ -1,5 +1,42 @@
 package com.kfc.restorater.model
 
-class RestaurantModel {
+import android.os.Parcel
+import android.os.Parcelable
 
+class RestaurantModel() : Parcelable {
+    private var name: String? = null
+    private var address: String? = null
+    private var phone: String? = null
+
+    constructor(parcel: Parcel) : this() {
+        name = parcel.readString()
+        address = parcel.readString()
+        phone = parcel.readString()
+    }
+
+    constructor(name: String, address: String, phone: String?) : this() {
+        this.name = name
+        this.address = address
+        this.phone = phone
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(address)
+        parcel.writeString(phone)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<RestaurantModel> {
+        override fun createFromParcel(parcel: Parcel): RestaurantModel {
+            return RestaurantModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<RestaurantModel?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
