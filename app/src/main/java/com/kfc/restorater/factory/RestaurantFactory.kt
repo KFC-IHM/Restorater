88d7f8d6d16c4d6a065aa63f647e88b7754c1c9c
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-05-09 16:13:07 +0200

    feat: update restaurant factory

--- app/src/main/java/com/kfc/restorater/factory/RestaurantFactory.kt
+++ app/src/main/java/com/kfc/restorater/factory/RestaurantFactory.kt
@@ -1,9 +1,17 @@
 package com.kfc.restorater.factory
 
+import com.kfc.restorater.model.restaurant.Restaurant
+
 class RestaurantFactory {
     companion object {
-        fun createRestaurant(name: String, address: String, phone: String?): RestaurantModel {
-            return RestaurantModel(name, address, phone)
+        fun createRestaurant(
+            id: Int? = null,
+            name: String,
+            description: String,
+            image: String? = null,
+            owner: Int,
+        ): Restaurant {
+            return Restaurant(id, name, description, image, owner)
         }
     }
 }
