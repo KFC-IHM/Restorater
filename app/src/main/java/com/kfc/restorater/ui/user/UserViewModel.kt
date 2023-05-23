6c7852f2c1b40e4cdfc731e61c1a2b09104b6361
Ozeliurs <ozeliurs@gmail.com>
2023-05-24 00:10:36 +0200

    feat: retrieve full user data on login
    Ref. #18

--- app/src/main/java/com/kfc/restorater/ui/user/UserViewModel.kt
+++ app/src/main/java/com/kfc/restorater/ui/user/UserViewModel.kt
@@ -3,29 +3,17 @@ package com.kfc.restorater.ui.user
 import androidx.databinding.BaseObservable
 import androidx.databinding.ObservableField
 import com.kfc.restorater.data.LoginRepository
-import com.kfc.restorater.repo.RetrofitWebServiceGenerator
-import com.kfc.restorater.repo.api.UserRepo
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 
 class UserViewModel(val loginRepository: LoginRepository) : BaseObservable() {
 
     val username = ObservableField("Bonjour, User!")
-    private var userWebService = RetrofitWebServiceGenerator.createService(UserRepo::class.java)
 
     init {
-        loginRepository.user.get()?.let {
-            userWebService.getUser(it.userId)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                { user ->
-                    username.set("Bonjour, ${user.username}!")
-                },
-                { _ ->
-                    username.set("Bonjour, ${it.userId}!")
-                }
-            )
-        }
+        loginRepository.userData.addOnPropertyChangedCallback(object :
+            androidx.databinding.Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(sender: androidx.databinding.Observable?, propertyId: Int) {
+                username.set("Bonjour, " + loginRepository.userData.get()?.username + " !")
+            }
+        })
     }
 }
