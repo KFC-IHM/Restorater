e8a17fb1e59fe49ec12a18d18d18f8142e0ea85c
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-05-28 10:09:05 +0200

    feat: show current location on map start
    ref: #3

--- app/src/main/java/com/kfc/restorater/ui/location/LocationFragment.kt
+++ app/src/main/java/com/kfc/restorater/ui/location/LocationFragment.kt
@@ -1,12 +1,19 @@
 package com.kfc.restorater.ui.location
 
-import androidx.fragment.app.Fragment
-
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -14,21 +21,59 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.kfc.restorater.R
 
-class LocationFragment : Fragment() {
 
+class LocationFragment : Fragment() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private fun getLocation(callback: (LatLng) -> Unit) {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
+            .addOnSuccessListener { location ->
+                val currentLatLng = location
+                    ?.let { LatLng(it.latitude, it.longitude) }
+                    ?: LatLng(0.0, 0.0)
+
+                callback(currentLatLng)
+            }
+    }
+
+
+    @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        Log.e("LocationFragment", "callback")
+
+
+        getLocation { currentLatLng: LatLng ->
+            googleMap.addMarker(
+                MarkerOptions().position(currentLatLng).title("Current Location")
+            )
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 20f))
+        }
+    }
+
+    private fun requestPermissions() {
+        val requestMultiplePermissions =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                permissions.entries.forEach {
+                    Log.d("LocationFragment", "${it.key} = ${it.value}")
+                }
+            }
+
+        requestMultiplePermissions.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
     }
 
     override fun onCreateView(
@@ -41,6 +86,8 @@ class LocationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        requestPermissions()
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
