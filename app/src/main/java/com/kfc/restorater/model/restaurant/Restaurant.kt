7efb2a44d2ded4307543f7a9889c7b592f940306
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-06-06 19:33:25 +0200

    feat: add lat/lng to Restaurant model
    Ref: #3

--- app/src/main/java/com/kfc/restorater/model/restaurant/Restaurant.kt
+++ app/src/main/java/com/kfc/restorater/model/restaurant/Restaurant.kt
@@ -1,6 +1,7 @@
 package com.kfc.restorater.model.restaurant
 
 import android.os.Parcelable
+import com.google.android.gms.maps.model.LatLng
 import com.kfc.restorater.model.review.Review
 import kotlinx.parcelize.Parcelize
 
@@ -12,10 +13,16 @@ data class Restaurant(
     val image: String? = null,
     val owner: Int,
     val review_set: List<Review>,
+    val latitude: Double,
+    val longitude: Double,
 ) : Parcelable {
 
     fun rating(): Float {
         return review_set.map { it.rating }.average().toFloat()
+    }
+
+    fun toLatLng(): LatLng {
+        return LatLng(latitude, longitude)
     }
 }
 
