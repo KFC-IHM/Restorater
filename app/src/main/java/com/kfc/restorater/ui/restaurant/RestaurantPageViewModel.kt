0b3ec4285de93736b8944512a483c4ea35e956f5
Ozeliurs <ozeliurs@gmail.com>
2023-06-07 21:29:27 +0200

    feat: add intent to google maps
    Ref. #28

--- app/src/main/java/com/kfc/restorater/ui/restaurant/RestaurantPageViewModel.kt
+++ app/src/main/java/com/kfc/restorater/ui/restaurant/RestaurantPageViewModel.kt
@@ -3,5 +3,4 @@ package com.kfc.restorater.ui.restaurant
 import androidx.databinding.BaseObservable
 import com.kfc.restorater.data.RestaurantRepository
 
-class RestaurantPageViewModel(val restaurantRepository: RestaurantRepository) : BaseObservable() {
-}
+class RestaurantPageViewModel(val restaurantRepository: RestaurantRepository) : BaseObservable() {}
